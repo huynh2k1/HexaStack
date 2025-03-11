d29b3c277cc11511597790464fe94fb9b4926d8b
Huynh <huynhnv@solarapp.asia>
2025-03-11 17:30:22 +0700

    init project

---
+++ Assets/Scripts/GridTests.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GridTests : MonoBehaviour
+{
+    [SerializeField] private Grid grid;
+
+    [Header("SETTINGS")]
+    [SerializeField] private Vector3Int gridPos;
+
+    private void OnValidate()
+    {
+        UpdatePostion();
+    }
+
+    private void UpdatePostion()
+    {
+        transform.position = grid.CellToWorld(gridPos);
+    }
+}
